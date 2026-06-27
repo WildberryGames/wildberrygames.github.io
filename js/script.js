438e1a7f084b8494ab46fd745182fc34a4e06426
SydneyTheKitty <180483676+SydneyTheKitty@users.noreply.github.com>
2026-06-27 15:27:44 -0600

    added nav bar functionality

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
 window.onscroll = function() { scrollEvent() };
 
 function scrollEvent() {
-    console.log("hello world");
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        //document.getElementById("subheader-1").classList.add("scrolled");
         document.getElementById("subheader-2").classList.add("scrolled");
+        document.getElementById("mainNav").classList.add("scrolled");
+        var links = document.getElementsByClassName("top-link");
+        for (let item of links) changeScrollOn(item);
     }
     else {
-        //document.getElementById("subheader-1").classList.remove("scrolled");
         document.getElementById("subheader-2").classList.remove("scrolled");
+        document.getElementById("mainNav").classList.remove("scrolled");
+        var links = document.getElementsByClassName("top-link");
+        for (let item of links) changeScrollOff(item);
     }
+}
+
+function changeScrollOn(item) {
+    item.classList.add("scrolled");
+}
+
+function changeScrollOff(item) {
+    item.classList.remove("scrolled");
 }
