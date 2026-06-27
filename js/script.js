afa0cb746f6af14fd66e856ecf7f340c5defd2ed
SydneyTheKitty <180483676+SydneyTheKitty@users.noreply.github.com>
2026-06-27 15:02:16 -0600

    A little push

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ window.onscroll = function() { scrollEvent() };
 function scrollEvent() {
     console.log("hello world");
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("subheader-1").classList.add("scrolled");
+        //document.getElementById("subheader-1").classList.add("scrolled");
         document.getElementById("subheader-2").classList.add("scrolled");
     }
     else {
-        document.getElementById("subheader-1").classList.remove("scrolled");
+        //document.getElementById("subheader-1").classList.remove("scrolled");
         document.getElementById("subheader-2").classList.remove("scrolled");
     }
 }
