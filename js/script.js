88d969d29ac55f81acae03e060adb01bc2f84e8b
SydneyTheKitty <180483676+SydneyTheKitty@users.noreply.github.com>
2026-07-02 14:37:33 -0600

    fixed the header

--- js/script.js
+++ js/script.js
@@ -2,14 +2,16 @@ window.onscroll = function() { scrollEvent() };
 
 function scrollEvent() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("subheader-2").classList.add("scrolled");
+        document.getElementById("subheaderBar").classList.add("scrolled");
         document.getElementById("mainNav").classList.add("scrolled");
+        document.getElementById("homeButton").classList.add("scrolled");
         var links = document.getElementsByClassName("top-link");
         for (let item of links) changeScrollOn(item);
     }
     else {
-        document.getElementById("subheader-2").classList.remove("scrolled");
+        document.getElementById("subheaderBar").classList.remove("scrolled");
         document.getElementById("mainNav").classList.remove("scrolled");
+        document.getElementById("homeButton").classList.remove("scrolled");
         var links = document.getElementsByClassName("top-link");
         for (let item of links) changeScrollOff(item);
     }
